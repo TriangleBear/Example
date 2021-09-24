c48ea545c306a8c21fc2e71c982d6751919226d7
aliyahsgbn <72782496+alysgbn@users.noreply.github.com>
2021-09-24 18:26:35 +0800

    try lang

--- test.py
+++ test.py
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
+
 print("hello charles")
 print("Hello kurt")
 print("Hello yvette")
-=======
 print("Hello World")
 print("Hello everyone")
 print("Hello Universe")
 print("Hello po")
-
-<<<<<<< HEAD
 print("hi guys yaw ko na maging programmer")
->>>>>>> 96c5caee73723267d4dc4f3daa26b58b3c8abeca
-=======
-
 print("hi guys yaw ko na maging programmer")
 print("-charles") 
->>>>>>> 6732d9a7231be0d55d1f642d7c6f69c45903e770
+print("try naten ulet ") 
+
+
